18a98c4489b738a1f33682e2fa9ac99f755a1a34
Skye-ye <Sky_Zheng_@outlook.com>
2025-04-07 07:33:33 +0000

    Add libpython2.7 and libncurses5 in Dockerfile to support gdb

--- arch/src/lib.rs
+++ arch/src/lib.rs
@@ -6,10 +6,5 @@
 #[cfg(feature = "riscv64")]
 mod riscv64;
 
-#[cfg(feature = "loongarch")]
-mod loongarch64;
-
-#[cfg(feature = "loongarch")]
-pub use loongarch64::*;
 #[cfg(feature = "riscv64")]
 pub use riscv64::*;
